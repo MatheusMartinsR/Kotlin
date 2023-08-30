71c67aaa0f969c71d64c9d2a0200b37027e96925
Logon Aluno <logonrmlocal@fiap.com.br>
2023-08-30 20:28:22 -0300

    CarroSalario

---
+++ CarroSalario/index.kt
@@ -0,0 +1,22 @@
+import kotlin.math.*
+
+fun main(){
+
+    var salario: Float = 5000f
+    var contas: Float = 3000f
+
+    var carro1: String = "Jetta GLI"
+    var carro2: String = "São Paulo"
+
+    if(salario > contas){
+        println("Que bom")
+    } else{
+        println("Que pena")
+    }
+
+    if (carro1 == carro2 ){
+        println("String iguais")
+    } else{
+        println("String diferentes")
+    }
+}
